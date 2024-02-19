60d1c18b765e8aa308a805fa543c88510882a85e
AziZaitulMukaramah <159240082+AziZaitulMukaramah@users.noreply.github.com>
2024-02-19 15:25:47 +0700

    Update data_mahasiswa.php
    Update Data Semester

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -2,5 +2,6 @@
 echo "<h1>Data Mahasiswa</h1>";
 echo "Nim : 22110043 <br>";
 echo "Nama Lengkap : Azi Zaitul Mukaramah";
-echo "Kelas : 22 MI-B";
+echo "Kelas : 22 MI-B <br>";
+echo "Semester Genap";
 ?>
